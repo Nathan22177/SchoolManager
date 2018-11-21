9b33571c35a998d98cbccb6013dc730d1c6ab02c
Nathan22177 <kokorevalery@gmail.com>
2018-11-21 16:39:40 +0200

    Set of skills required for different lessons

---
+++ src/main/java/Proficiency.java
@@ -0,0 +1,7 @@
+public enum Proficiency {
+    NATIVE_LANGUAGE,        //Teacher is able to teach native language;
+    FOREIGN_LANGUAGE,       //to teach foreign language;
+    MATH,                   //to teach math;
+    SOCIAL_STUDIES,         //to teach social studies;
+    COACHING                //to be student's group assigned curator;
+}
