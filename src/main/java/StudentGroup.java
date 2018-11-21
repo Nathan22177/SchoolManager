1d633041274f90f617b734426a58a6b8edd0cc6b
Nathan22177 <kokorevalery@gmail.com>
2018-11-21 16:46:42 +0200

    A group of students of particular grade and neededclassers

--- src/main/java/StudentGroup.java
+++ src/main/java/StudentGroup.java
@@ -1,2 +1,45 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class StudentGroup {
+    private Long groupID;
+    private int year;
+    private String flow;
+    private Set students = new HashSet();
+
+    public StudentGroup() {
+
+    }
+
+    public Long getGroupID() {
+        return groupID;
+    }
+
+    public void setGroupID(Long groupID) {
+        this.groupID = groupID;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getFlow() {
+        return flow;
+    }
+
+    public void setFlow(String flow) {
+        this.flow = flow;
+    }
+
+    public Set getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set students) {
+        this.students = students;
+    }
 }
