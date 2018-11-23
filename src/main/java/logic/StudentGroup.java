6a0d655be9ce1ba77bcacfaff5f65be69830720a
Nathan22177 <kokorevalery@gmail.com>
2018-11-23 23:18:02 +0200

    Adding demo class Main and few adjustments

--- src/main/java/logic/StudentGroup.java
+++ src/main/java/logic/StudentGroup.java
@@ -7,18 +7,24 @@ import java.util.Set;
 @Entity
 @Table(name = "STUDENT_GROUP")
 public class StudentGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "GROUP_ID", length = 10, nullable = false)
     private Long groupID;
+
+    @Column(name = "GRADE", length = 5)
     private int year;
+    @Column(name = "STUDENT_FLOW", length = 5)
     private String flow;
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "GROUP_COURSES", joinColumns = {@JoinColumn(name = "GROUP_ID")}, inverseJoinColumns = {@JoinColumn(name = "SUBJECT_ID")})
     private Set courses = new HashSet();
 
     public StudentGroup() {
 
     }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "GROUP_ID", length = 10, nullable = false)
+
     public Long getGroupID() {
         return groupID;
     }
@@ -26,7 +32,7 @@ public class StudentGroup {
     public void setGroupID(Long groupID) {
         this.groupID = groupID;
     }
-    @Column(name = "GRADE", length = 5)
+
     public int getYear() {
         return year;
     }
@@ -34,7 +40,7 @@ public class StudentGroup {
     public void setYear(int year) {
         this.year = year;
     }
-    @Column(name = "STUDENT_FLOW", length = 5)
+
     public String getFlow() {
         return flow;
     }
@@ -43,8 +49,6 @@ public class StudentGroup {
         this.flow = flow;
     }
 
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "GROUP_COURSES", joinColumns = {@JoinColumn(name = "GROUP_ID")}, inverseJoinColumns = {@JoinColumn(name = "SUBJECT_ID")})
     public Set getCourses() {
         return courses;
     }
