7a397bab78520f1cdcb3e4d87e25d80635af12a4
Nathan22177 <kokorevalery@gmail.com>
2018-11-25 16:50:35 +0200

    Class that describes @Subject objects.

--- src/main/java/logic/Subject.java
+++ src/main/java/logic/Subject.java
@@ -4,20 +4,34 @@ import lombok.Data;
 
 import javax.persistence.*;
 
+//Annotating Hibernate entity
 @Entity
+
+//Passing typical boilerplate to lombok
 @Data
+
+//Declaring a table for subjects
 @Table(name = "SUBJECT")
+/**
+ * This class does not stores neither sets of classes, nor sets of teachers,
+ * and used only as a descriptor for courses thus simplifying assignment of teachers
+ * and student groups for scheduling.
+ */
 public class Subject {
 
+
+    //Here we are storing ID of each course(subject). This may come in handy for scheduling.
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "SUBJECT_ID")
     private int subjectID;
 
+    //Name of the course respectively
     @Column(name = "SUBJECT_NAME")
     private String subjectName;
 
+    //To simplify assignment of a teacher for particular course
     @Column(name = "REQUIRED_PROFICIENCY")
-    private Proficiency requiredProficiency; //logic.Skill teacher has to have to be able to teach this subject
+    private Proficiency requiredProficiency;
 
 }
