f83a91169ed52a5fea7b9e9a1556f80843a3128a
Nathan22177 <kokorevalery@gmail.com>
2018-11-25 16:50:57 +0200

    Class that describes @StudentGroup objects.

--- src/main/java/logic/StudentGroup.java
+++ src/main/java/logic/StudentGroup.java
@@ -6,11 +6,20 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+//Annotating Hibernate entity.
 @Entity
+
+//Passing typical boilerplate to lombok.
 @Data
+
+//Declaring a table for groups of students.
 @Table(name = "STUDENT_GROUP")
 public class StudentGroup {
 
+    /*
+     * Here we are storing id of particular group of students, it may come handy for scheduling courses,
+     * exams, merging groups into flow, and assigning them to internship.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "GROUP_ID", length = 10, nullable = false)
@@ -22,6 +31,10 @@ public class StudentGroup {
     @Column(name = "STUDENT_FLOW", length = 5)
     private String flow;
 
+    /**
+    * Since EACH group may (and most probably will) attend SEVERAL courses we're choosing
+    * to relate groups to courses (subjects) as ONE group to MANY courses.
+    */
     @OneToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "GROUP_COURSES", joinColumns = {@JoinColumn(name = "GROUP_ID")}, inverseJoinColumns = {@JoinColumn(name = "SUBJECT_ID")})
     private Set<Subject> subjects = new HashSet<>();
