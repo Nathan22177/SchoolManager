bba3e601d2b8ff4e0eaaa081bb67799e85a6b6e7
Nathan22177 <kokorevalery@gmail.com>
2018-11-25 16:51:11 +0200

    Class that describes @Skill objects.

--- src/main/java/logic/Skill.java
+++ src/main/java/logic/Skill.java
@@ -4,17 +4,27 @@ import lombok.Data;
 
 import javax.persistence.*;
 
+//Annotating Hibernate entity.
 @Entity
+
+//Passing all typical boilerplate to lombok.
 @Data
+
+//Declaring table for all specific skills (gender history/calculus/artificial intelligence/etc.).
 @Table(name = "SKILL")
 public class Skill {
 
     @Column(name = "SKILL_NAME")
     private String name;
 
+    //Generified proficiency associated with particular skill.
     @Column(name = "SKILL_REQUIRED")
-    private Proficiency skill;
+    private Proficiency proficiency;
 
+    /*
+     * Here we are storing id of particular skill, it may come handy for tracking teachers
+     * capable to teach within a very specific fields.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "SKILL_ID", length = 10, nullable = false)
